f5e3a519c5f5eff951b3da5f04a611c59c2ec21d
faizank09855 <fk09855@gmail.com>
2022-12-30 00:16:49 +0530

    ejs file and render methods added

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -3,6 +3,16 @@ const path = require('path');
 
 exports.controller = (req , res) => {
 
-    res.sendFile(path.join(__dirname , ".." , "view" , "user-page.html")); 
-    
+
+    res.render('user-page' , {
+userName : "faizan" , 
+pageTitle : "User Page" , 
+bookName : "Book Name" ,
+category : "Category" ,
+artist : "Artist" , 
+
+
+    }) ; 
+    // res.sendFile(path.join(__dirname , ".." , "view" , "user-page.html")); 
+
     }
--- first-app.js
+++ first-app.js
@@ -4,6 +4,9 @@ const adminRoutes = require('./routes/admin') ;
 const userRoutes = require('./routes/user') ; 
 const errorRoutes = require('./routes/error-path') ; 
 let app = express();
+
+app.set('view engine' , 'ejs');
+app.set('views' , 'view') ; 
 app.use(express.static(path.join(__dirname , "style")))
 app.use(adminRoutes.Router) 
 app.use(userRoutes.Router) ;
