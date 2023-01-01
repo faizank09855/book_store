a80e192ae6861c5b8cc7100281de08f9980109a0
faizank09855 <fk09855@gmail.com>
2023-01-02 01:31:05 +0530

    model added

--- controller/admin_controller.js
+++ controller/admin_controller.js
@@ -1,10 +1,24 @@
 const express = require('express') ;
 const path = require('path'); 
-
+const Product = require('../model/product');
 exports.adminController = (req , res)=>{
-
+    const fetchData = Product.fetchAll(); 
     res.render('admin-page' ,
      {title : 'Admin Page' ,
+     fetchData :  fetchData,
+     isAdmin : true ,
+    isUser : false ,
+    }) ; 
+    
+}
+
+exports.adminFetchData = (req , res , next)=>{
+    const product = new Product("faizan");
+    product.save(); 
+    const fetchData = Product.fetchAll(); 
+    res.render('admin-page' ,
+     {title : 'Admin Page' ,
+     fetchData : fetchData ?? [],
      isAdmin : true ,
     isUser : false ,
     }) ; 
---
+++ model/product.js
@@ -0,0 +1,19 @@
+const products = [];
+
+module.exports = class Product{
+
+    constructor(title) {
+        this.title = title ;
+    }
+
+save (){
+    products.push(this);
+    console.log(products);
+}
+
+static fetchAll() {
+    console.log(products);
+return products ; 
+}
+
+}
