4b7771f48afbff601f60ee7e597f73502fe0e8db
faizank09855 <fk09855@gmail.com>
2023-01-06 00:31:58 +0530

    code reformated

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userController = require('../controller/user_controller');
 const routes = express.Router();
+
 routes.get('/user', userController.controller);
 exports.Router = routes; 
